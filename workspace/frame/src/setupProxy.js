edbf5bff2f808abce1345b78ea9b4785f4b7498b
Jonathan Sheely <jsheely@thirtytech.net>
2021-08-19 12:33:17 -0400

    Initial

---
+++ workspace/frame/src/setupProxy.js
@@ -0,0 +1,31 @@
+const {
+  createProxyMiddleware,
+  responseInterceptor,
+} = require('http-proxy-middleware')
+
+module.exports = function (app) {
+  app.use(
+    '/client',
+    createProxyMiddleware({
+      target: 'http://localhost:5001',
+      changeOrigin: true,
+      followRedirects: true,
+      onProxyReq: (proxyReq, req, res) => {
+        console.log('request')
+      },
+      selfHandleResponse: true,
+      onProxyRes: responseInterceptor(
+        async (responseBuffer, proxyRes, req, res) => {
+          return responseBuffer
+        },
+      ),
+    }),
+  )
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5002',
+      changeOrigin: true,
+    }),
+  )
+}
